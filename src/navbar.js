b08b52ca56a30443fda12b9266f0f4e26c471287
rajeshmn47 <rajeshmn47@gmail.com>
2022-03-19 17:06:55 +0530

    last commit

---
+++ src/navbar.js
@@ -0,0 +1,19 @@
+import SearchIcon from '@material-ui/icons/Search';
+import stack from './images/stackoverflow.jpeg'
+
+export const Navbar=()=>{
+    return(
+<>
+<div className="navbar flex align-center justify-center">
+    <img src={stack}  alt='stacl'/>
+    <h5 className='m-1'>Stack<span className='font-bold'>Overflow</span></h5>
+    <p className='opacity-50 m-1'>Products</p>
+    <div className='bg-white w-1/2 height-5 p-1 m-2 border-2 border-white-600'>
+        <SearchIcon style={{opacity:'0.5'}}/>
+    <input className="w-30 h-5 navbarinput" placeholder="search..."/>
+    </div>
+</div>
+</>
+    )
+}
+export default Navbar
---
+++ src/question.js
@@ -0,0 +1,22 @@
+export const Question=()=>{
+    return(
+<>
+<div className="question">
+<div className='questio'> 
+<h5 style={{fontSize:'1.5vmax',width:'10vw'}}>o votes</h5><div style={{color:'blue'
+,width:'35vw'}}>  length parameter seems not working</div></div>
+<div className="questio">
+<h5 style={{fontSize:'1.5vmax',width:'10vw'}}>o answers</h5><div style={{color:'blue'
+,display:'flex',width:'25vw'}}><div className="tag">javascript</div><div className="tag">css</div>
+<div className="tag">java</div><div style={{color:'blue',fontSize:'1vmax',width:'10vw'}}>rajesh</div></div>
+
+</div>
+<div className="questio">
+<h5 style={{fontSize:'1vmax',width:'10vw'}}>1 view</h5>
+<div style={{width:'35vw'}}></div>
+</div>
+</div>
+</>
+    )
+}
+export default Question
