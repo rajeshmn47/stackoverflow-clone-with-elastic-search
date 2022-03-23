e3eeca13a7fb65ef8e453572aab1add94346a5de
rajesh <rajeshmn47@gmail.com>
2022-03-24 02:44:12 +0530

    netlify deploy

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,53 +1,74 @@
 import axios from 'axios'
-import { REGISTER_USER_SUCCESS,REGISTER_USER_REQUEST,
-    LOGIN_SUCCESS,LOGIN_REQUEST,LOGIN_FAIL, REGISTER_USER_FAIL,
-    LOAD_USER_SUCCESS,LOAD_USER_REQUEST,LOAD_USER_FAIL
- } from '../constants/userConstants';
+import {
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_REQUEST,
+  LOGIN_FAIL,
+  REGISTER_USER_FAIL,
+  LOAD_USER_SUCCESS,
+  LOAD_USER_REQUEST,
+  LOAD_USER_FAIL,
+} from '../constants/userConstants'
 
- const headers = {
-    'Accept': 'application/json'
-};
-export const register= (myform) => async (dispatch) => {
-    try{
+const headers = {
+  Accept: 'application/json',
+}
+export const register = (myform) => async (dispatch) => {
+  try {
     console.log(myform)
-    dispatch({ type:REGISTER_USER_REQUEST  });
-    const {data}=await axios.post('http://localhost:8000/auth/register',{myform})
+    dispatch({ type: REGISTER_USER_REQUEST })
+    const { data } = await axios.post(
+      'https://stackoverflowclonerajesh.herokuapp.com/auth/register',
+      { myform }
+    )
     console.log(data)
-    localStorage.setItem("server_token",data.server_token);
-    dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
-}
-catch (error) {
-    console.log( error.response,'asdfgh')
-    dispatch({ type: REGISTER_USER_FAIL, payload: error.response.data.message });
+    localStorage.setItem('server_token', data.server_token)
+    dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user })
+  } catch (error) {
+    console.log(error.response, 'asdfgh')
+    dispatch({ type: REGISTER_USER_FAIL, payload: error.response.data.message })
   }
 }
 
-export const login= (myform) => async (dispatch) => {
-  try{  console.log(myform)
-    dispatch({ type:LOGIN_REQUEST  });
-    const {data}=await axios.post('http://localhost:8000/auth/login',{myform})
+export const login = (myform) => async (dispatch) => {
+  try {
+    console.log(myform)
+    dispatch({ type: LOGIN_REQUEST })
+    const { data } = await axios.post(
+      'https://stackoverflowclonerajesh.herokuapp.com/auth/login',
+      { myform }
+    )
     console.log(data)
-    localStorage.setItem("server_token",data.server_token);
-    dispatch({ type: LOGIN_SUCCESS, payload: data.user });
-}
-catch (error) {
-    console.log( error.response,'asdfgh')
-    dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
+    localStorage.setItem('server_token', data.server_token)
+    dispatch({ type: LOGIN_SUCCESS, payload: data.user })
+  } catch (error) {
+    console.log(error.response, 'asdfgh')
+    dispatch({ type: LOGIN_FAIL, payload: error.response.data.message })
   }
 }
 
-
-
-  export const loadUser = () => async (dispatch) => {
-    try {
-        console.log('rajivya')
-        const servertoken=localStorage.getItem('server_token')&&localStorage.getItem('server_token') 
-      dispatch({ type: LOAD_USER_REQUEST });
-   const {data}=  await axios("http://localhost:8000/auth/loaduser", 
-      {method:'get',headers: { ...headers,'Content-Type': 'application/json',servertoken:servertoken }})
-  console.log(data.message[0])
-      dispatch({ type: LOAD_USER_SUCCESS, payload: data.message[0] });
-    } catch (error) {
-      dispatch({ type: LOAD_USER_FAIL, payload: error.response.data.message });
-    }
-  };
+export const loadUser = () => async (dispatch) => {
+  try {
+    console.log('rajivya')
+    const servertoken =
+      localStorage.getItem('server_token') &&
+      localStorage.getItem('server_token')
+    dispatch({ type: LOAD_USER_REQUEST })
+    const { data } = await axios(
+      'https://stackoverflowclonerajesh.herokuapp.com/auth/loaduser',
+      {
+        method: 'get',
+        headers: {
+          ...headers,
+          'Content-Type': 'application/json',
+          servertoken: servertoken,
+        },
+      }
+    )
+    console.log(data.message[0])
+    dispatch({ type: LOAD_USER_SUCCESS, payload: data.message[0] })
+  } catch (error) {
+    dispatch({ type: LOAD_USER_FAIL, payload: error.response.data.message })
+  }
+}
